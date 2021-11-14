811e10f4bfc9a9a8dc3c25c475dd692c89f0d64d
Jason <jasonlinfl@gmail.com>
2021-11-14 16:56:20 -0500

    addition

--- src/test/java/ucf/assignments/ItemTest.java
+++ src/test/java/ucf/assignments/ItemTest.java
@@ -2,19 +2,29 @@ package ucf.assignments;
 
 import org.junit.jupiter.api.Test;
 
+import java.text.ParseException;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ItemTest {
 
+    Item item = new Item("food", "2021-08-15");
+
+    ItemTest() throws ParseException {
+    }
+
     @Test
     void getDescription() {
+        assertEquals(item.getDescription(), "food");
     }
 
     @Test
     void getTime() {
+        assertEquals(item.getTime(),"2021-08-15");
     }
 
     @Test
     void getComplete() {
+        assertEquals(item.getComplete(), "false");
     }
 }
